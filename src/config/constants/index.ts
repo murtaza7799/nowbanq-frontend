c7a310616ef8f1b2492a1dcf9631f74f35796544
Muhammad Murtaza <48488384+murtaza7799@users.noreply.github.com>
2022-05-24 23:18:34 +0500

    router address updated according to new contract

--- src/config/constants/index.ts
+++ src/config/constants/index.ts
@@ -1,7 +1,7 @@
 import { ChainId, JSBI, Percent, Token } from '@pancakeswap/sdk'
 import { mainnetTokens, testnetTokens } from './tokens'
 
-export const ROUTER_ADDRESS = '0x928630a5164D2E4A827D265d2a0C16af0C0265fE'
+export const ROUTER_ADDRESS = '0xBe8a0915A1FdCc8179E1AB64Ee35980bd30AE919'
 
 
 // a list of tokens by chain
